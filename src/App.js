5e4c87b5fa3d85b8217a10d496a49bc37180ae56
taichigr <79818659+taichigr@users.noreply.github.com>
2021-06-09 21:00:16 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 
-const App = props => {
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
+const App = (props) => {
+  const [state, setState] = useState(props);
+  const { name, price } = state;
 
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
+  useEffect(() => {
+    console.log("this is like componentDidMount or componentDidupdate.");
+  });
+  useEffect(() => {
+    console.log("this is like componentDidMount.");
+  }, []);
+
+  useEffect(() => {
+    console.log("this is for name only.");
+  }, [name]);
 
   return (
     <>
-      <p>現在の{name}は、{price}円です。</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>reset</button>
-      <input value={name} onChange={e => setName(e.target.value)} />
-
-
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
+      <button onClick={() => setState(props)}>reset</button>
+      <input
+        value={name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
     </>
-  )
-
-}
+  );
+};
 
 App.defaultProps = {
-  name: 'サンプル',
+  name: "サンプル",
   price: 1000
-}
-export default App
+};
+export default App;
